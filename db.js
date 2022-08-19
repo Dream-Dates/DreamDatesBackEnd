993a6e90630c5f3753df31b9f3f0077863d2cf22
damian kohut <damiankohut@gmai.com>
2022-08-19 18:49:31 -0400

    change db

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
-const Pool = require("pg").Pool;
+// const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  user: "postgres",
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "damian",
+//   host: "localhost",
+//   port: 5432,
+//   database: "dreamdates",
+// });
+
+// module.exports = pool;
+require('dotenv').config();
+const { Pool } = require('pg')
+
+
+const connectionDevelopment = {
+  user:"postgres",
+  database: 'dreamdates',
   password: "damian",
   host: "localhost",
-  port: 5432,
-  database: "dreamdates",
-});
+  port: 5432
+};
+
+const connectionProduction = {
+  connectionString: process.env.DATABASE_URL, 
+  ssl: {rejectUnauthorized: false}
+}
+
+const pool = new Pool(process.env.NODE_ENV === 'production' ? connectionProduction : connectionDevelopment)
 
 module.exports = pool;
