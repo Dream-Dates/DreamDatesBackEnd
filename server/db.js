d9c234c2ed233cf116a6084331ee369deae105b6
damian kohut <damiankohut@gmai.com>
2022-08-19 16:42:02 -0400

    changed database info

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "Alvin",
+  password: "damian",
   host: "localhost",
   port: 5432,
   database: "dreamdates",
